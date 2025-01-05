7da23c1189e71450a291be1b3037b0e42198542e
arraychen <2110796846@qq.com>
2025-01-05 16:20:16 +0800

    项目基本结构已经搭建

---
+++ Mall-ShopX/Mall-applet/src/main/java/com/itcast/mallapplet/MallAppletApplication.java
@@ -0,0 +1,13 @@
+package com.itcast.mallapplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAppletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAppletApplication.class, args);
+    }
+
+}
---
+++ Mall-ShopX/Mall-applet/src/test/java/com/itcast/mallapplet/MallAppletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcast.mallapplet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAppletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
